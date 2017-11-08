f6f0f959f2dc53aca1fdbf69b8d36c07f3bd9db5
maynagashev <lifesup@gmail.com>
2017-11-08 10:30:53 +0700

    + do login, and goto main page
    Take and save screenshots with current datetime.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,36 @@
 const puppeteer = require('puppeteer');
 const config = require('./config');
+//console.log(config);
 
-console.log(config);
 (async () => {
     const browser = await initBrowser();
     const page = await openProfilePage(browser);
 
+    // goto login
     await page.click('a.login-link.btn-clear');
     await page.waitForNavigation();
 
-    await wrappingUp(page, browser);
+    // do login
+    await page.focus('#email');
+    await page.keyboard.type(config.userEmail);
+    await page.focus('#password');
+    await page.keyboard.type(config.userPassword);
+    await page.click('#submit-button');
+    await page.waitForNavigation();
+    await takeScreenshot(page, browser);
+
+    // goto main page
+    await page.goto("https://stackoverflow.com/");
+    await takeScreenshot(page, browser);    
+    
+    return await browser.close();
 })();
 
 async function initBrowser() {
     let browser = await puppeteer.launch({
         headless: false,
         args: ["--window-size=1920,1080"],
-        slowMo: 250,
+        //slowMo: 250node,
     });
     console.log('Chromimum executablePath:', puppeteer.executablePath());
     return browser;
@@ -29,7 +43,8 @@ async function openProfilePage(browser) {
     return page;
 }
 
-async function wrappingUp(page, browser) {
-    await page.screenshot({ path: config.screenPath });
-    return await browser.close();
+async function takeScreenshot(page, browser) {
+    let date = (new Date()).toISOString().replace(/\:/g, '-');
+    let path = config.screenPath.replace(/\.png/, '-' + date + '.png');
+    return await page.screenshot({ path: path });
 }
