69e3255b2cec1877109023bce0bde9a0c999115b
maynagashev <lifesup@gmail.com>
2017-11-08 10:34:02 +0700

    add credentials to config.example

--- src/config.example.js
+++ src/config.example.js
@@ -2,4 +2,7 @@
 module.exports = {
     profileUrl: "https://stackoverflow.com/users/123123/testuser?tab=profile",
     screenPath: "screens/stackoverflow-profile.png",
+
+    userEmail: "",
+    userPassword: "",
 }
